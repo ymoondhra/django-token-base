f6efdef71270eb3f1a0691613a2cbd2cc309cfeb
Yash Moondhra <ymoondhra@gmail.com>
2019-08-17 11:09:09 -0700

    rough draft for version 1

---
+++ users/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+
+from . import models, serializers
+from .serializers import UserSerializer
+
+
+class UserListView(generics.ListCreateAPIView):
+    queryset = models.CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateAPIView):
+    queryset = models.CustomUser.objects.all()
+    serializer_class = UserSerializer
